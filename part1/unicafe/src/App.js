e44abe8501252df977305d90d4558faf74c830c7
Dzemal Kurtic <Dzemaludeen@gmail.com>
2021-11-28 13:43:13 +0100

    exercise 1.7 finished

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const [numberOfClicks, SetNumberOfClicks] = useState(0)
+  const [allClicksSum, setAllClickSum] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [percentGood, setPercentGood] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    SetNumberOfClicks(numberOfClicks + 1)
+    setAllClickSum(allClicksSum + 1)
+    setAverage((allClicksSum + 1) / (numberOfClicks + 1))
+    setPercentGood((good + 1) / (numberOfClicks + 1) * 100)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    SetNumberOfClicks(numberOfClicks + 1)
+    setAverage(allClicksSum / (numberOfClicks + 1))
+    setPercentGood(good / (numberOfClicks + 1) * 100)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    SetNumberOfClicks(numberOfClicks + 1)
+    setAllClickSum(allClicksSum - 1)
+    setAverage((allClicksSum - 1) / (numberOfClicks + 1))
+    setPercentGood(good / (numberOfClicks + 1) * 100)
+  }
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+      <Statistics
+        goodRating={good}
+        neutralRating={neutral}
+        badRating={bad}
+        numOfClicks={numberOfClicks}
+        avg={average}
+        percentPositive={percentGood} />
+    </div>
+  )
+}
+
+const Statistics = ({ goodRating, neutralRating, badRating, numOfClicks, avg, percentPositive }) => {
+  return (
+    <>
+      <h2>statistics</h2>
+      <p>good {goodRating}</p>
+      <p>neutral {neutralRating}</p>
+      <p>bad {badRating}</p>
+      <p>all {numOfClicks}</p>
+      <p>average {avg}</p>
+      <p>positive {percentPositive} %</p>
+    </>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+export default App
